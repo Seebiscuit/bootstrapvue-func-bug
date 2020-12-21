9e9dbfe20604154c3e58af7269d28dcc04deaf9c
Gary Kaganas <kaganasg@gmail.com>
2020-12-20 20:14:47 -0500

    Using class syntanx and breaking things

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
   <div id="app">
     <div id="nav">
+      <b-icon
+        icon="gear-fill"
+        aria-hidden="true"
+      ></b-icon>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <b-calendar
+      v-model="value"
+      @context="onContext"
+      locale="en-US"
+    ></b-calendar>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+/* This DOES NOT Work */
+export default class App extends Vue {
+  public data() {
+    return {
+      value: "",
+    };
+  }
+}
+
+//* This Works:
+/*
+export default Vue.extend({
+  data() {
+    return {
+      value: "",
+    };
+  },
+});
+*/
+</script>
 
 <style>
 #app {
